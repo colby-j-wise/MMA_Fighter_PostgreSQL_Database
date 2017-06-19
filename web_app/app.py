45b5cf00c0846b97494c4d421bad8a2d39fe6960
colby-j-wise <colby.j.wise@gmail.com>
2017-06-19 18:23:16 -0400

    fixing app.py so runs on virtual machine

--- web_app/app.py
+++ web_app/app.py
@@ -306,7 +306,7 @@ def results():
 					f1_res = "%s, %s" % (result, method)
 				else:
 					f1_res = result
-                        elif result == "win":
+			elif result == "win":
 				if method:
 					f1_res = "loss, %s" % method
 			elif result == "draw":
@@ -658,5 +658,31 @@ def get_age(birthdate):
 	return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
 
 if __name__== "__main__":
-	app.run()
+	import click
+
+	@click.command()
+	@click.option('--debug', is_flag=True)
+	@click.option('--threaded', is_flag=True)
+	@click.argument('HOST', default='0.0.0.0')
+	@click.argument('PORT', default=8111, type=int)
+	def run(debug, threaded, host, port):
+	"""
+	This function handles command line parameters.
+	Run the server using:
+
+        python server.py
+
+	Show the help text using:
+
+	python server.py --help
+
+	"""
+	
+	HOST, PORT = host, port
+	print "running on %s:%d" % (HOST, PORT)
+	app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+run()
+
+
 
